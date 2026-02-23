e085a4df2fb5158ae4468fc037b1ecca1cf37ca5
Girish Chavidi <girishchavidi425@gmail.com>
2026-02-23 11:51:25 +0530

    Fixed About.css import path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../styles/About.css";
+import "../styles/about.css";
 import { portfolioData } from "../data/portfolioData";
 
 const About = () => {
